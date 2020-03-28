0bc98156e8a3ecf23508547f9064c7e31d6c0b88
Josh Sauder <joshsauder@gmail.com>
2020-03-28 00:33:55 -0400

    grid changes to dijkstra's

--- algorithms/Dijkstra.ts
+++ algorithms/Dijkstra.ts
@@ -1,67 +1,70 @@
-import Heap from 'heap'
-import {node} from '../models/Graph'
+import Heap from 'heap';
+import { node } from '../models/Graph'
 
-export default class Dijkstra {
-    
-    findShortestPath(start: node, end: node, grid: node[][]): node[]{
 
-        //sort by weight
-        let queuedNodes = new Heap<node>(function(a, b){
-            return a.weight - b.weight;
+export function findShortestPath(start: node, end: node, grid: node[][]): node[]{
+
+    console.log(grid)
+
+    //sort by weight
+    let queuedNodes = new Heap<node>(function (a, b) {
+        return a.weight - b.weight;
+    })
+
+    let visitedNodes: node[] = []
+
+    //set starting node weight to 0
+    start.weight = 0
+    queuedNodes.push(start)
+
+
+
+    while (queuedNodes.size() > 0) {
+        let openNode = queuedNodes.pop()
+
+        if (openNode === end) { return visitedNodes }
+
+        let neighborNodes = findNeighborNodes(openNode, grid[0].length, grid.length, grid);
+
+        neighborNodes.forEach(neighbor => {
+            if (!neighbor.closed) {
+                let weight = openNode.weight + 1
+                neighbor.weight = weight
+
+                queuedNodes.push(neighbor)
+            }
         })
-        
-        let visitedNodes: node[] = []
 
-        //set starting node weight to 0
-        start.weight = 0
-        queuedNodes.push(start)
-
-
-
-        while(queuedNodes.size() > 0){
-            let openNode = queuedNodes.pop()
-
-            if(openNode === end){return visitedNodes}
-
-            let neighborNodes = this.findNeighborNodes(openNode, grid.length, grid[0].length, grid);
-
-            neighborNodes.forEach(neighbor => {
-                if(!neighbor.closed){
-                    let weight = openNode.weight + 1
-                    neighbor.weight = weight
-
-                    queuedNodes.push(neighbor)
-                }
-            })
-
-            openNode.closed = true
-            visitedNodes.push(openNode)
-        }
-
-        return []
+        openNode.closed = true
+        visitedNodes.push(openNode)
     }
 
-
-    findNeighborNodes(node: node, width: number, height: number, allNodes: node[][]): node[]{
-        let neighbors: node[] = []
-
-        if(node.x + 1 < width){
-            neighbors.push(allNodes[node.y][node.x+1])
-        }
-
-        if(node.x - 1 > -1){
-            neighbors.push(allNodes[node.y][node.x-1])
-        }
-
-        if(node.y + 1 < height){
-            neighbors.push(allNodes[node.y+1][node.x])
-        }
-
-        if(node.y - 1 > -1){
-            neighbors.push(allNodes[node.y-1][node.x])
-        }
-
-        return neighbors.filter(node => node.closed == false)
-    }
-
+    return []
 }
+
+
+function findNeighborNodes(node: node, width: number, height: number, allNodes: node[][]): node[] {
+    let neighbors: node[] = []
+
+    if (node.x + 1 < width) {
+        neighbors.push(allNodes[node.y][node.x+1])
+    }
+
+    if (node.x - 1 > -1) {
+        neighbors.push(allNodes[node.y][node.x-1])
+    }
+
+    if (node.y + 1 < height) {
+        neighbors.push(allNodes[node.y+1][node.x])
+    }
+
+    if (node.y - 1 > -1) {
+        neighbors.push(allNodes[node.y-1][node.x])
+    }
+
+    console.log(neighbors)
+
+
+    return neighbors.filter(node => node.closed == false)
+}
+
--- components/Grid/Grid.tsx
+++ components/Grid/Grid.tsx
@@ -2,10 +2,13 @@ import React, {Component} from 'react'
 import {FlatList} from 'react-native';
 import Item from "./Item"
 import {node} from '../../models/Graph';
+import {findShortestPath} from '../../algorithms/Dijkstra'
 
 interface State {
     numCols: number,
     selected: Map<string, boolean>
+    start: node,
+    end: node,
     graph: node[][]
 }
 
@@ -17,6 +20,8 @@ export class Grid extends Component<Props, State> {
         super(props)
         this.state = { 
             numCols: 10, 
+            start: undefined,
+            end: undefined,
             selected: new Map(),
             graph: []
         }
@@ -26,12 +31,18 @@ export class Grid extends Component<Props, State> {
         this.setupGrid()
     }
 
+    componentDidUpdate(){
+        if(this.state.start && this.state.end){
+            console.log(findShortestPath(this.state.start, this.state.end, this.state.graph))
+        }
+    }
+
     setupGrid = () => {
         this.setState(state => {
             for(let r = 0; r < 20; r++){
                 let row: node[] = []
                 for(let c = 0; c < this.state.numCols; c++){
-                    row.push({key: `${r},${c}`,x: r, y: c, weight: Infinity, closed: false})
+                    row.push({key: `${c},${r}`,x: c, y: r, weight: Infinity, closed: false})
                 }
                 state.graph.push(row)
             }
@@ -41,12 +52,15 @@ export class Grid extends Component<Props, State> {
     }
 
     itemSelected = (id: string) => {
-        this.setState(state => {
-            state.selected.set(id, !state.selected.get(id))
-            return state
-        })
+        let coordinates = id.split(',')
+        let selectedNode = {x: parseInt(coordinates[1]), y: parseInt(coordinates[0]), key: id, weight: Infinity, closed: false}
+        if(this.state.start === undefined){this.setState({start: {...selectedNode}})} 
+        else if(this.state.start.key === id){this.setState({start: undefined})} 
+        else {this.setState({end: {...selectedNode}})}
 
-        if(this.state.selected.size == 2){}
+        this.setState(state =>{
+            state.selected.set(id, !state.selected.get(id))
+        })
     }
 
     renderData = (): node[] => {
@@ -54,7 +68,7 @@ export class Grid extends Component<Props, State> {
         this.state.graph.forEach(row => {
             nodes = nodes.concat(row)
         })
-        
+
         return nodes
     }
  
@@ -62,7 +76,8 @@ export class Grid extends Component<Props, State> {
         return (
             <FlatList 
             data= {this.renderData()}
-            renderItem={({item}) => <Item id={item.key} onSelect={this.itemSelected} selected={!!this.state.selected.get(item.key)}/>}
+            renderItem={({item}) => 
+            <Item id={item.key} onSelect={this.itemSelected} selected={!!this.state.selected.get(item.key)}/>}
             numColumns={this.state.numCols}
             extraData={this.state}
             keyExtractor={(item) => item.key}
